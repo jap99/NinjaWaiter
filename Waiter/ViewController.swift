34ebf72cb0909cd89979e9dd67e2c609338a6777
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro-2.local>
2018-05-16 08:21:33 -0700

    first screen UI

---
+++ Waiter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Waiter
+//
+//  Created by Javid Poornasir on 5/16/18.
+//  Copyright © 2018 Javid Poornasir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
