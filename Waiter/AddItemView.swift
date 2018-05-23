a36e6802bc43b834b260c66bd865b089fefc2798
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro-2.local>
2018-05-22 22:13:36 -0700

    Created the UI for the AddItemView

---
+++ Waiter/AddItemView.swift
@@ -0,0 +1,36 @@
+//
+//  AddItemView.swift
+//  Waiter
+//
+//  Created by Javid Poornasir on 5/22/18.
+//  Copyright © 2018 Javid Poornasir. All rights reserved.
+//
+
+import UIKit
+
+class AddItemView: UIView, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addItemImageView: UIImageView!
+    @IBOutlet weak var tv: UITableView!
+    @IBOutlet weak var orderButton: UIButton!
+    
+
+    // MARK: - TABLE VIEW
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
