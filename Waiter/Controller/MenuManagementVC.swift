4533effb8591bbd23102f2bd3cdf64ee73115404
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro-2.local>
2018-06-09 12:25:42 -0700

    UI work

---
+++ Waiter/Controller/MenuManagementVC.swift
@@ -0,0 +1,50 @@
+//
+//  MenuManagementVC.swift
+//  Waiter
+//
+//  Created by Javid Poornasir on 6/9/18.
+//  Copyright © 2018 Javid Poornasir. All rights reserved.
+//
+
+import UIKit
+
+class MenuManagementVC: UIViewController {
+
+    @IBOutlet weak var menuManagementButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var addCategoryButton: UIButton!
+    @IBOutlet weak var tv: UITableView!
+    
+    @IBOutlet weak var addCategoryView: UIView!
+    @IBOutlet weak var foodTextField: UITextField!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+
+    // MARK: - IBACTIONS
+    
+    @IBAction func backButton_Pressed(_ sender: Any) {
+    }
+    
+    @IBAction func settingsButton_Pressed(_ sender: Any) {
+    }
+    
+    @IBAction func menuManagementButton_Pressed(_ sender: Any) {
+    }
+    
+    @IBAction func addCategoryButton_Pressed(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButton_Pressed(_ sender: Any) {
+    }
+
+    @IBAction func addButton_Pressed(_ sender: Any) {
+    }
+    
+
+}
---
+++ Waiter/View/CategoryCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCell.swift
+//  Waiter
+//
+//  Created by Javid Poornasir on 6/9/18.
+//  Copyright © 2018 Javid Poornasir. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    
+}
