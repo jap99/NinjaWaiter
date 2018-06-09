3fb2399056853265527f2c000a71d1c22f3479a1
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro-2.local>
2018-06-09 10:02:11 -0700

    update

---
+++ Waiter/Controller/PrinterVC.swift
@@ -0,0 +1,21 @@
+//
+//  PrinterVC.swift
+//  Waiter
+//
+//  Created by Javid Poornasir on 6/9/18.
+//  Copyright © 2018 Javid Poornasir. All rights reserved.
+//
+
+import UIKit
+
+class PrinterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    
+
+}
