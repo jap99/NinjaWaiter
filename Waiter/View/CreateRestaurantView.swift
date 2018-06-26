022125c190ad1404af1afda570dcfacc036fa85f
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro-2.local>
2018-06-25 19:42:50 -0700

    code cleanup

--- Waiter/View/CreateRestaurantView.swift
+++ Waiter/View/CreateRestaurantView.swift
@@ -46,49 +46,68 @@ class CreateRestaurantView: UIView {
                     _currentUser = AppUser(user:currentUser)
                     
                     DataService.instance.mainRef.child(FIR_ADMINISTRATORS).child(_currentUser.uid).observe(.value, with: { (obj) in
+                       
                         if let _ = obj.value as? String {
                             _currentUser.type = .adamin
+                        
                         } else {
                             _currentUser.type = .staff
                         }
                     })
-                    _userDefault.set(self.emailTextField.text!, forKey:kUsername)
-                    _userDefault.set(self.passwordTextField.text!, forKey:kPassword)
-                    _userDefault.synchronize()
                     
-                    let ac = UIAlertController(title: APP_NAME, message: CREATE_ACCOUNT_MESSAGE, preferredStyle: .alert)
+                    self.saveUserLocally()
                     
-                    self.viewController.present(ac, animated: true, completion: { [weak self] in
-                        
-                        DispatchQueue.main.asyncAfter(deadline: .now(), execute: {
-                           
-                            ac.dismiss(animated: true, completion: {
-                                self?.removeFromSuperview()
-                                
-                                // Send user to SettingsVC
-                                let viewController = self?.viewController.storyboard?.instantiateViewController(withIdentifier: "DashboardVC-ID") as! DashboardVC
-                                self?.viewController.present(viewController, animated: true, completion: nil)
-                            })
-                        })
-                    })
-                     self.removeFromSuperview()
+                    self.createAccount_Alert()
+
                 } else {
                     // Handle Error Case
-                    let alertController = UIAlertController(title: APP_NAME, message: errorMessage ?? DEFAULT_ERROR_MESSAGE, preferredStyle: UIAlertControllerStyle.alert)
-                    let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action: UIAlertAction) in
-                        self.removeFromSuperview()
-                    })
-                    let retryAction = UIAlertAction(title: "Retry", style: UIAlertActionStyle.cancel, handler: { (action: UIAlertAction) in
-                    })
-                    alertController.addAction(okAction)
-                    alertController.addAction(retryAction)
-                    self.viewController.present(alertController, animated: true, completion: nil)
+                    retry_Alert()
                 }
             }
-           
         }
-        
     }
     
+    // MARK: - ACTIONS
+    
+    func saveUserLocally() {
+        _userDefault.set(self.emailTextField.text!, forKey:kUsername)
+        _userDefault.set(self.passwordTextField.text!, forKey:kPassword)
+        _userDefault.synchronize()
+    }
+    
+    // MARK: - ALERTS
+    
+    func retry_Alert() {
+        
+        let alertController = UIAlertController(title: APP_NAME, message: errorMessage ?? DEFAULT_ERROR_MESSAGE, preferredStyle: UIAlertControllerStyle.alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action: UIAlertAction) in
+            self.removeFromSuperview()
+        })
+        let retryAction = UIAlertAction(title: "Retry", style: UIAlertActionStyle.cancel, handler: { (action: UIAlertAction) in
+        })
+        alertController.addAction(okAction)
+        alertController.addAction(retryAction)
+        self.viewController.present(alertController, animated: true, completion: nil)
+    }
+    
+    func createAccount_Alert() {
+        
+        let ac = UIAlertController(title: APP_NAME, message: CREATE_ACCOUNT_MESSAGE, preferredStyle: .alert)
+        
+        self.viewController.present(ac, animated: true, completion: { [weak self] in
+            
+            DispatchQueue.main.asyncAfter(deadline: .now(), execute: {
+                
+                ac.dismiss(animated: true, completion: {
+                    self?.removeFromSuperview()
+                    
+                    // Send user to SettingsVC
+                    let viewController = self?.viewController.storyboard?.instantiateViewController(withIdentifier: "DashboardVC-ID") as! DashboardVC
+                    self?.viewController.present(viewController, animated: true, completion: nil)
+                })
+            })
+        })
+        self.removeFromSuperview()
+    }
     
 }
