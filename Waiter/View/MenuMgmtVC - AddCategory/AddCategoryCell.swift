81f0b4db3cc1f5851f2807ab990e91257d30a900
Javid Poornasir <javidpoornasir@Javids-MacBook-Air.local>
2018-07-21 21:09:18 -0700

    MenuMgmtVC - screen for small devices - constraints

--- Waiter/View/MenuMgmtVC - AddCategory/AddCategoryCell.swift
+++ Waiter/View/MenuMgmtVC - AddCategory/AddCategoryCell.swift
@@ -15,5 +15,20 @@ class AddCategoryCell: UITableViewCell {
     @IBOutlet weak var renameButton: UIButton!
     @IBOutlet weak var deleteButton: UIButton!
     
-   
+    override func awakeFromNib() {
+            configureInterfaceForSmallDevice()
+    }
+    
+    func configureInterfaceForSmallDevice() {
+        if UIScreen.main.bounds.width < 1030 {
+            let cellViews: [String: UIView] = ["s": categoryNumber, "t": categoryTitle, "r": renameButton, "d": deleteButton]
+            categoryNumber.translatesAutoresizingMaskIntoConstraints = false
+            categoryTitle.translatesAutoresizingMaskIntoConstraints = false
+            renameButton.translatesAutoresizingMaskIntoConstraints = false
+            deleteButton.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[s]-70-[t]-90-[r]-60-[d]-35-|", options: [], metrics: nil, views: cellViews) 
+                ].flatMap{$0})
+        }
+    }
 }
--- Waiter/View/MenuMgmtVC - AddItem/AddItemCell.swift
+++ Waiter/View/MenuMgmtVC - AddItem/AddItemCell.swift
@@ -18,59 +18,57 @@ class AddItemCell: UITableViewCell {
     @IBOutlet weak var lunchSwitch: UISwitch!
     @IBOutlet weak var dinnerSwitch: UISwitch!
     
-    
     override func awakeFromNib() {
-        
-        let cellViews: [String: UIView] = ["c": categoryTitle, "bs": breakfastSwitch, "ls": lunchSwitch, "ds": dinnerSwitch]
-        categoryTitle.translatesAutoresizingMaskIntoConstraints = false
-        breakfastSwitch.translatesAutoresizingMaskIntoConstraints = false
-        lunchSwitch.translatesAutoresizingMaskIntoConstraints = false
-        dinnerSwitch.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            
-            NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[c]-70-[bs]-90-[ls]-60-[ds]-50-|", options: [], metrics: nil, views: cellViews),
-            NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[c]", options: [], metrics: nil, views: cellViews)
-            ].flatMap{$0})
+        configureInterfaceForSmallDevice()
     }
+    
     
     // MARK: - ACTIONS
     
     func addAndRemoveCategoryItem(switch: UISwitch, categoryUID: String, catName: String) {
-        
         if let arrItem = parentVC.dictOfArrays[categoryUID] {
-            
             if arrItem.contains(catName) {
                 let index = arrItem.index(of:catName)
                 parentVC.dictOfArrays[categoryUID]!.remove(at:index!)
             } else {
                 parentVC.dictOfArrays[categoryUID]!.append(catName)
             }
-        
         } else {
             parentVC.dictOfArrays[categoryUID]  = [catName]
         }
     }
     
+    func configureInterfaceForSmallDevice() {
+        if UIScreen.main.bounds.width < 1030 {
+            let cellViews: [String: UIView] = ["c": categoryTitle, "bs": breakfastSwitch, "ls": lunchSwitch, "ds": dinnerSwitch]
+            categoryTitle.translatesAutoresizingMaskIntoConstraints = false
+            breakfastSwitch.translatesAutoresizingMaskIntoConstraints = false
+            lunchSwitch.translatesAutoresizingMaskIntoConstraints = false
+            dinnerSwitch.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[c]-70-[bs]-90-[ls]-60-[ds]-50-|", options: [], metrics: nil, views: cellViews),
+                NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[c]", options: [], metrics: nil, views: cellViews)
+                ].flatMap{$0})
+        }
+    }
+    
+    
     // MARK: - IBACTIONS
     
     @IBAction func breakfastSwitchAction(_ sender: UISwitch) {
         if let uidOfCategorySelected = Singleton.sharedInstance.categoriesItems[sender.tag].uid {
-            
             addAndRemoveCategoryItem(switch:sender, categoryUID: uidOfCategorySelected, catName: "Breakfast")
         }
     }
     
     @IBAction func lunchSwitch(_ sender: UISwitch) {
         if let uidOfCategorySelected = Singleton.sharedInstance.categoriesItems[sender.tag].uid {
-            
             addAndRemoveCategoryItem(switch:sender, categoryUID: uidOfCategorySelected, catName: "Lunch")
         }
     }
     
     @IBAction func dinnerSwitch(_ sender: UISwitch) {
         if let uidOfCategorySelected = Singleton.sharedInstance.categoriesItems[sender.tag].uid {
-            
             addAndRemoveCategoryItem(switch:sender, categoryUID: uidOfCategorySelected, catName: "Dinner")
         }
     }
